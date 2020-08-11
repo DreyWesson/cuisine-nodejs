01ccb8fecb2b3916c8ed1b197890d8274b5f7e4e
dreyWesson <dreyWesson@gmail.com>
2020-08-11 18:46:45 +0100

    dummy footer

--- main.js
+++ main.js
@@ -13,11 +13,11 @@ router.get("/index.html", (req, res) => {
   res.writeHead(httpStatus.OK, contentTypes.html);
   utils.getFile("views/index.html", res);
 });
-router.get("/courses.html", (req, res) => {
+router.get("/courses", (req, res) => {
   res.writeHead(httpStatus.OK, contentTypes.html);
   utils.getFile("views/courses.html", res);
 });
-router.get("/contact.html", (req, res) => {
+router.get("/contact", (req, res) => {
   res.writeHead(httpStatus.OK, contentTypes.html);
   utils.getFile("views/contact.html", res);
 });
